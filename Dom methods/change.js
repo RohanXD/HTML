f34934a0594157025eaf0ccfa297710ee8402196
RohanXD <rohanprajapati121212@gmail.com>
2024-11-08 13:52:40 +0530

    Initial commit

---
+++ Dom methods/change.js
@@ -0,0 +1,16 @@
+function text(){
+    document.getElementById("hello").textContent="JavaScript Class";
+}
+function change() {
+    document.body.style.backgroundColor = "yellow";
+}
+document.addEventListener("contextmenu",myFun)
+function myFun() {
+  document.getElementById("image").src="https://imgs.search.brave.com/-WydhaJfqPl4aediQyLnuNbjcFISgTEN2_798xhk734/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZS5zaHV0dGVyc3Rv/Y2suY29tL2ltYWdl/LXBob3RvL2N1dGUt/Y2F0LXRpbHRpbmct/aGVhZC1iYWJ5LTI2/MG53LTI0OTI0MDM0/NjcuanBn";
+}
+function hide(){
+    document.getElementById("hid").style.display="none";
+}
+function show(){
+    document.getElementById("hid").style.display="block";
+}
---
+++ Dom methods/change2.js
@@ -0,0 +1,54 @@
+function changeheading(){
+    var head=document.getElementsByTagName("h2");
+    for(i=0; i<head.length ; i++)
+    {
+        head[i].textContent="cccccchange heading";
+    }
+}
+function changecolor(){
+    var list=document.getElementsByTagName("li");
+    for(i=0; i<list.length ; i++)
+    {
+        list[i].style.backgroundColor="magenta";
+    }
+}
+function hidediv(){
+    var divs=document.getElementsByTagName("div");
+    for(i=0; i<divs.length ; i++)
+    {
+        divs[i].style.display="none";
+    }
+}
+function show(){
+    var divs=document.getElementsByTagName("div");
+    for(i=0; i<divs.length ; i++)
+    {
+        divs[i].style.display="block";
+    }
+}
+
+function pborder(){
+    var para=document.getElementsByTagName("p");
+    for(i=0; i<para.length ; i++)
+    {
+        para[i].style.border="10px solid lime";
+    }
+}
+function changesize(){
+    var span=document.getElementsByTagName("p");
+    for(i=0; i<span.length ; i++)
+    {
+        span[i].style.fontSize="50px";
+    }
+}
+function queryS(){
+        var qy=document.querySelectorAll('.para')
+        for(i=0;i<qy.length;i++)
+        {
+            qy[i].style.backgroundColor="yellow"
+        }
+    }
+function query(){
+        document.querySelector('h1').style.fontSize="50px"
+        document.querySelector('h1').style.color="green"
+}
